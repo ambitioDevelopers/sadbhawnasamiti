e0b352510fe00669111cf216ad99b80959c696f0
ambitioDevelopers <37516733+ambitioDevelopers@users.noreply.github.com>
2018-07-18 03:58:55 +0530

    all links updates
    --gallery page thumnail not updated

---
+++ js/act.js
@@ -0,0 +1,18 @@
+    $("#expand1").show();
+	$("#expand2").show();
+	$("#expand3").show();
+	$("#expand4").show();
+      
+          jQuery("#expand1").click(function(){
+              $("#expand1").hide();
+            });
+            
+          jQuery("#expand2").click(function(){
+			  $("#expand2").hide();
+		  });
+	  jQuery("#expand3").click(function(){
+			  $("#expand3").hide();
+		  });
+	  jQuery("#expand4").click(function(){
+			  $("#expand4").hide();
+		  });
