88572bc49414a7ac308156e5400d41cdb7b198bd
ambitioDevelopers <37516733+ambitioDevelopers@users.noreply.github.com>
2018-07-23 02:28:06 +0530

    fixes
    --custom.js
    --MIS added

--- js/custom.js
+++ js/custom.js
@@ -9,17 +9,6 @@ $(window).load(function() {
 $(function(){
     "use strict";
     /*=========================================================================
-            One Page Nav
-    =========================================================================*/
-    $(".navigation").onePageNav({
-        currentClass: 'current',
-        changeHash: false,
-        scrollSpeed: 700,
-        scrollThreshold: 0.5,
-        easing: 'easeInOutCubic'
-    });
-
-    /*=========================================================================
             Portfolio filter
     =========================================================================*/
       if($('#works .item-outer').length > 0){
@@ -40,73 +29,11 @@ $(function(){
     });
 
     /*=========================================================================
-            Bootstrap Tooltip
-    =========================================================================*/
-    $(".resume-download").tooltip();
-
-    /*=========================================================================
-            Carousels / Resume, Testimonials, Customers /
-    =========================================================================*/
-      $(".customer-carousel").owlCarousel({
-        items: 4
-      });
-
-      $(".resume-carousel, .testimonial-carousel").owlCarousel({
-        singleItem:true
-      });
-
-    /*=========================================================================
-            Backstretch Background Slider
-    =========================================================================*/
-    $("#welcome").backstretch([
-        "http://placehold.it/1200x768",
-        "http://placehold.it/1200x768",
-        // "assets/images/your_pic.jpg" 
-    ], {duration: 5000, fade: 400});
-
-    /*=========================================================================
             Welcome & Header Height
     =========================================================================*/
     $("#welcome").css({'height':($(window).height())+'px'});
     $(".header").css({'height':($(window).height())+'px'});
 
-    /*=========================================================================
-            Magnific Popup Functions
-    =========================================================================*/
-    $('.work-image').magnificPopup({
-      type: 'image'
-    });
-
-    $('.work-video').magnificPopup({
-      type: 'iframe',
-      iframe: {
-          markup: '<div class="mfp-iframe-scaler">'+
-                    '<div class="mfp-close"></div>'+
-                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                  '</div>', 
-
-          patterns: {
-            youtube: {
-              index: 'youtube.com/',
-
-              id: 'v=',
-
-              src: 'http://www.youtube.com/embed/%id%?autoplay=1'
-            },
-            vimeo: {
-              index: 'vimeo.com/',
-              id: '/',
-              src: '//player.vimeo.com/video/%id%?autoplay=1'
-            },
-            gmaps: {
-              index: '//maps.google.',
-              src: '%id%&output=embed'
-            }
-
-          },
-
-          srcAction: 'iframe_src',
-        }
-    });
+   
 
 });
